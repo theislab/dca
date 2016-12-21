231c3d2811a788b436162af9dcc56603bdae4d9a
Gökcen Eraslan <goekcen.eraslan@helmholtz-muenchen.de>
2016-12-21 16:05:47 +0100

    Load save model in encoder

--- autoencoder/encode.py
+++ autoencoder/encode.py
@@ -1,17 +1,17 @@
 from .network import autoencoder
-from .io import read_records
+from .io import read_records, load_model
 
 
-def encode(inputfile, weights, hiddensize=100):
+def encode(input_file, log_dir, hiddensize=100):
 
-    X = read_records(inputfile)
+    X = read_records(input_file)
     size = X.shape[1]
 
-    model, encoder, _ = autoencoder(size, hidden_size=hiddensize)
-    model.load_weights(weights, by_name=False)
-
+    model = load_model(log_dir)
+    return model
 
 
 def encode_with_args(args):
-    encode(inputfile=args.dataset, weights=args.weightfile,
-            hiddensize=args.hiddensize)
+    encode(input_file = args.dataset,
+           log_dir=args.logdir,
+           hiddensize=args.hiddensize)
