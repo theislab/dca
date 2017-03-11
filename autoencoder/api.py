53a418a3fc108b045e86fcba2e27d9c904b16bd3
Gökçen Eraslan <gokcen.eraslan@gmail.com>
2017-03-11 12:31:14 +0100

    Add a simple API to make autoencoding programmatically easy

---
+++ autoencoder/api.py
@@ -0,0 +1,17 @@
+from .io import preprocess
+from .train import train
+from .encode import encode
+
+
+def autoencode(count_matrix, kfold=None,
+               censor_matrix=None, type='normal',
+               learning_rate=1e-2,
+               hidden_size=10,
+               epochs=10):
+
+    x = preprocess(count_matrix, kfold=kfold, censor=censor_matrix)
+    model = train(x, hidden_size=hidden_size, learning_rate=learning_rate,
+                  aetype=type, epochs=epochs)
+    encoded = encode(count_matrix, model)
+
+    return encoded
