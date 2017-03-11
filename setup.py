0a3ae5b4e954d647e3f635ba95b8bcccff3d2fc9
Gökçen Eraslan <gokcen.eraslan@gmail.com>
2017-03-11 11:46:17 +0100

    Add tf>=1.0.0 to setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from subprocess import check_output, CalledProcessError
 try:
     num_gpus = len(check_output(['nvidia-smi', '--query-gpu=gpu_name',
                                  '--format=csv']).decode().strip().split('\n'))
-    tf = 'tensorflow-gpu' if num_gpus > 1 else 'tensorflow'
+    tf = 'tensorflow-gpu>=1.0.0' if num_gpus > 1 else 'tensorflow>=1.0.0'
 except CalledProcessError:
     tf = 'tensorflow>=1.0.0'
 except FileNotFoundError:
