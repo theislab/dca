1f90a3d733de99cc9c412cdd559ed3ad26519acc
Gökçen Eraslan <gokcen.eraslan@gmail.com>
2017-03-12 09:42:18 +0100

    Add reduce option to API

--- autoencoder/api.py
+++ autoencoder/api.py
@@ -3,7 +3,7 @@ from .train import train
 from .encode import encode
 
 
-def autoencode(count_matrix, kfold=None,
+def autoencode(count_matrix, kfold=None, reduced=False,
                censor_matrix=None, type='normal',
                learning_rate=1e-2,
                hidden_size=10,
@@ -12,6 +12,6 @@ def autoencode(count_matrix, kfold=None,
     x = preprocess(count_matrix, kfold=kfold, censor=censor_matrix)
     model = train(x, hidden_size=hidden_size, learning_rate=learning_rate,
                   aetype=type, epochs=epochs)
-    encoded = encode(count_matrix, model)
+    encoded = encode(count_matrix, model, reduced=reduced)
 
     return encoded
