9c1190133a680717850a4d0f46a96591b7be4e33
Gökçen Eraslan <gokcen.eraslan@gmail.com>
2017-03-13 12:40:30 +0100

    Change mask parameter in API.

--- autoencoder/api.py
+++ autoencoder/api.py
@@ -4,12 +4,12 @@ from .encode import encode
 
 
 def autoencode(count_matrix, kfold=None, reduced=False,
-               censor_matrix=None, type='normal',
+               mask=None, type='normal',
                learning_rate=1e-2,
                hidden_size=10,
                epochs=10):
 
-    x = preprocess(count_matrix, kfold=kfold, censor=censor_matrix)
+    x = preprocess(count_matrix, kfold=kfold, mask=mask)
     model = train(x, hidden_size=hidden_size, learning_rate=learning_rate,
                   aetype=type, epochs=epochs)
     encoded = encode(count_matrix, model, reduced=reduced)
