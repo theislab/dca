1c5d5a7889adef448951aee06495cdddd39cfae7
Gökçen Eraslan <gokcen.eraslan@gmail.com>
2017-04-09 21:32:45 +0200

    Create model in api using network module and pass to train function.

--- autoencoder/api.py
+++ autoencoder/api.py
@@ -1,17 +1,28 @@
 from .io import preprocess
 from .train import train
+from .network import autoencoder
 from .encode import encode
 
 
 def autoencode(count_matrix, kfold=None, reduced=False,
-               mask=None, type='normal',
-               learning_rate=1e-2, hidden_size=10, l2_coef=0.,
+               mask=None, type='normal', activation='relu',
+               learning_rate=1e-2, hidden_size=(256,64,256), l2_coef=0.,
                epochs=200, **kwargs):
 
     x = preprocess(count_matrix, kfold=kfold, mask=mask, testset=False)
-    model, losses = train(x, hidden_size=hidden_size, l2_coef=l2_coef,
-                          learning_rate=learning_rate, aetype=type,
-                          epochs=epochs, **kwargs)
+
+    model, encoder, decoder, loss, extras = \
+            autoencoder(x['shape'][1],
+                        hidden_size=hidden_size,
+                        l2_coef=l2_coef,
+                        activation=activation,
+                        masking=(mask is None),
+                        aetype=type)
+
+    losses = train(x, model,
+                   learning_rate=learning_rate,
+                   epochs=epochs, **kwargs)
+
     encoded = encode(count_matrix, model, reduced=reduced)
 
     return {'encoded': encoded,
