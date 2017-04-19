8b72eea6834546808756033f78f730042a57e77e
Gökcen Eraslan <goekcen.eraslan@helmholtz-muenchen.de>
2017-04-19 09:53:23 +0200

    Use TB callback in biochemist examples
    Former-commit-id: 2b8db89fb7344eea9808d3991d12ba527330d34c

--- data/test-biochemists-zinb.py
+++ data/test-biochemists-zinb.py
@@ -1,6 +1,7 @@
 import numpy as np
 from autoencoder.io import read_text
 from autoencoder.network import mlp
+from keras.callbacks import TensorBoard
 
 count = read_text('biochemists.tsv', header='infer')
 y = count[:, 0].astype(int)
@@ -8,9 +9,10 @@ x = count[:, 1:]
 
 net = mlp(x.shape[1], output_size=1, hidden_size=(), masking=False, loss_type='zinb')
 model = net['model']
+tb = TensorBoard(log_dir='./logs', histogram_freq=1)
 
 model.summary()
 model.compile(loss=net['loss'], optimizer='Adam')
-model.fit(x, y, epochs=700, batch_size=32)
+model.fit(x, y, epochs=700, batch_size=32, callbacks=[tb])
 
 print('Theta: %f' % (1/np.exp(model.get_weights()[2][0][0])))
