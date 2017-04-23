c1b4c6682920912fe58bef51884c3c751116e98f
Gökcen Eraslan <goekcen.eraslan@helmholtz-muenchen.de>
2017-04-23 18:01:46 +0200

    Fix new theta parameterization in ZINB loss.

--- data/test-biochemists-zinb.py
+++ data/test-biochemists-zinb.py
@@ -15,4 +15,4 @@ model.summary()
 model.compile(loss=net['loss'], optimizer='Adam')
 model.fit(x, y, epochs=700, batch_size=32, callbacks=[tb])
 
-print('Theta: %f' % (1/np.exp(model.get_weights()[2][0][0])))
+print('Theta: %f' % net['extra_models']['dispersion']())
