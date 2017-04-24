05fa888ad5a97e64dd301035cf35418acae3070b
Gökcen Eraslan <goekcen.eraslan@helmholtz-muenchen.de>
2017-04-24 15:19:19 +0200

    predict.predict is not needed anymore
    Former-commit-id: bddaf0dbd58563503666baf97653efcc015d018f

--- autoencoder/predict.py
+++ autoencoder/predict.py
@@ -3,26 +3,6 @@ import numpy as np
 
 from .io import read_from_file
 
-
-def predict(data, model, output_file=None, reduced=False):
-    size = data.shape[1]
-
-    assert model.input_shape[1] == size, \
-           'Input size of data and pretrained model must be same'
-
-    if reduced:
-        #FIXME
-        encoder = get_encoder(model)
-        predictions = encoder.predict(data)
-    else:
-        predictions = model.predict(data)
-
-    if output_file:
-        np.savetxt(output_file, predictions)
-
-    return predictions
-
-
 def predict_with_args(args):
 
     x = read_from_file(args.trainingset)
