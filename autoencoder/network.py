Fix the mode formula

autoencoder/network.py
@@ -218,7 +218,7 @@ class MLP(object):
             print('Calculating reconstructions...')
             res['mean'] = self.model.predict(count_matrix)
             m, d = res['mean'], res['dispersion']
-            res['mode'] = np.floor((m/(m+d))*((d-1)/d)).astype(np.int)
+            res['mode'] = np.floor(m*((d-1)/d)).astype(np.int)
             res['mode'][res['mode'] < 0] = 0
 
         if self.file_path: