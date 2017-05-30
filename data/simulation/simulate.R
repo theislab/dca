22917e9ac3155767748c43ffc15334e3da462f2a
Gökcen Eraslan <goekcen.eraslan@helmholtz-muenchen.de>
2017-05-30 09:30:22 +0200

    Save simulated files into dirs

--- data/simulation/simulate.R
+++ data/simulation/simulate.R
@@ -3,7 +3,7 @@
 # biocLite('splatter')
 library(splatter)
 
-save.sim <- function(sim) {
+save.sim <- function(sim, dir) {
   counts     <- counts(sim)
   truecounts <- get_exprs(sim, 'TrueCounts')
   dropout    <- get_exprs(sim, 'Dropout')
@@ -14,21 +14,26 @@ save.sim <- function(sim) {
   id <- deparse(substitute(sim))
 
   # save count matrices
-  write.table(counts, paste0('counts_', id, '.tsv'),
+  write.table(counts, paste0(dir, '/counts_', id, '.tsv'),
               sep='\t', row.names=F, col.names=F, quote=F)
-  write.table(truecounts, paste0('info_', id, '_truecounts.tsv'),
+  write.table(truecounts, paste0(dir, '/info_', id, '_truecounts.tsv'),
               sep='\t', row.names=F, col.names=F, quote=F)
 
   # save ground truth dropout labels
-  write.table(dropout, paste0('info_', id, '_droupout.tsv'),
+  write.table(dropout, paste0(dir, '/info_', id, '_droupout.tsv'),
               sep='\t', row.names=F, col.names=F, quote=F)
 
   # save metadata
-  write.table(cellinfo, paste0('info_', id, '_cellinfo.tsv'), sep='\t',
+  write.table(cellinfo, paste0(dir, '/info_', id, '_cellinfo.tsv'), sep='\t',
               row.names=F, quote=F)
-  write.table(geneinfo, paste0('info_', id, '_geneinfo.tsv'), sep='\t',
+  write.table(geneinfo, paste0(dir, '/info_', id, '_geneinfo.tsv'), sep='\t',
               row.names=F, quote=F)
 }
+
+if (!dir.exists('real/single')) dir.create('real/single', showWarnings=F, recursive=T)
+if (!dir.exists('real/group')) dir.create('real/group', showWarnings=F, recursive=T)
+if (!dir.exists('sim/single')) dir.create('sim/single', showWarnings=F, recursive=T)
+if (!dir.exists('sim/group')) dir.create('sim/group', showWarnings=F, recursive=T)
 
 #### Estimate parameters from the real dataset
 data(sc_example_counts)
@@ -39,24 +44,24 @@ sample_real_single <- splatSimulateSingle(params, groupCells=2000, nGenes=500,
                                           dropout.present=T, seed=42,
                                           bcv.common=2) # limit disp to get
                                                         # fewer true zeros
-save.sim(sample_real_single)
+save.sim(sample_real_single, 'real/single')
 
 # simulate data, two groups
 sample_real_group <- splatSimulateGroups(params, groupCells=c(1000, 1000),
                                          nGenes=500, dropout.present=T, seed=42,
                                          bcv.common=2)
-save.sim(sample_real_group)
+save.sim(sample_real_group, 'real/group')
 
 
 #### Simulate data with default params
 sample_sim_single <- splatSimulateSingle(groupCells=2000, nGenes=500,
                                          dropout.present=T, seed=42,
                                          dropout.shape=-0.5, dropout.mid=4)
-save.sim(sample_sim_single)
+save.sim(sample_sim_single, 'sim/single')
 
 # simulate data, two groups
 sample_sim_group <- splatSimulateGroups(groupCells=c(1000, 1000),
                                         nGenes=500, dropout.present=T,
                                         dropout.shape=-0.5, dropout.mid=4,
                                         seed=42)
-save.sim(sample_sim_group)
+save.sim(sample_sim_group, 'sim/group')
