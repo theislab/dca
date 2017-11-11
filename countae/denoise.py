Fix normalization

countae/denoise.py
@@ -22,15 +22,17 @@ import os
 from . import io
 from .network import AE_TYPES
 import numpy as np
+import torch
 
 
 def denoise_with_args(args):
+    torch.manual_seed(42)
 
-    ds = text_to_zarr(args.inputfile,
-                 output_file=os.path.join(args.outputdir, 'input.zarr'),
-                 transpose=args.transpose,
-                 test_split=args.testsplit,
-                 size_factors=args.normtype)
+    ds = io.text_to_zarr(args.inputfile,
+                         output_file=os.path.join(args.outputdir, 'input.zarr'),
+                         transpose=args.transpose,
+                         test_split=args.testsplit,
+                         size_factors=args.normtype)
 
     enc_size = [int(x) for x in args.enchiddensize.split(',') if x]
     enc_dropout = [float(x) for x in args.encdropoutrate.split(',')]
@@ -61,8 +63,14 @@ def denoise_with_args(args):
 
     print(net)
 
-    ret = net.train(X=X,
-                    Y=ds.train.matrix[:],
+    norm_options = io.NormOptions(sizefactors=args.sizefactors,
+                                  lognorm=args.loginput,
+                                  zeromean=args.zeromean)
+
+    X = io.normalize(ds.train.matrix[:], ds.train.size_factors, norm_options)
+    Y = ds.train.matrix[:]
+
+    ret = net.train(X=X, Y=Y,
                     epochs=args.epochs,
                     batch_size=args.batchsize,
                     l2=args.l2,
@@ -70,10 +78,18 @@ def denoise_with_args(args):
                     l2_out=args.l2out,
                     gpu=args.gpu)
 
+    X = io.normalize(ds.full.matrix[:], ds.full.size_factors, norm_options)
     net.predict(X,
                 rownames=ds.full.rownames,
                 colnames=ds.full.colnames,
                 folder=args.outputdir,
                 gpu=args.gpu)
 
-    #TODO: predict on test set if available
+    # predict on test set if available
+    if ds.test:
+        X = io.normalize(ds.test.matrix[:], ds.test.size_factors, norm_options)
+        net.predict(X,
+                    rownames=ds.test.rownames,
+                    colnames=ds.test.colnames,
+                    folder=os.path.join(args.outputdir, 'testset'),
+                    gpu=args.gpu)

countae/io.py
@@ -19,13 +19,14 @@ from __future__ import division
 from __future__ import print_function
 
 import pickle, os, numbers
+from collections import namedtuple
 
 import numpy as np
 import pandas as pd
 import zarr
 from sklearn.model_selection import train_test_split
 
-NormOptions = namedtuple('NormOptions', 'sizefactors lognorm zscore')
+NormOptions = namedtuple('NormOptions', 'sizefactors lognorm zeromean')
 
 class Matrix:
     def __init__(self, grp):
@@ -185,15 +186,17 @@ def estimate_size_factors(x, normtype='zheng'):
         raise NotImplemented
 
 
-def normalize(x, sf, logtrans=True, sfnorm=True, zeromean=True):
-    if sfnorm:
+def normalize(x, sf, norm_options):
+
+    #   logtrans=True, sfnorm=True, zeromean=True
+    if norm_options.sizefactors:
         assert len(sf.shape) == 1
         x = x / (sf[:, None]+1e-8)  # colwise div
 
-    if logtrans:
+    if norm_options.lognorm:
         x = np.log1p(x)
 
-    if zeromean:
+    if norm_options.zeromean:
         x = x - x.mean(axis=0)
 
     return x

countae/network.py
@@ -53,8 +53,7 @@ class Autoencoder():
                                                  cname=True,
                                                  act=torch.nn.Sequential)},
                  activation='ReLU',
-                 batchnorm=True,
-                 norm_options=NormOptions(sizefactors=True, lognorm=True, zscore=True)):
+                 batchnorm=True):
 
         assert loss_type in LOSS_TYPES, 'Undefined loss type'
 
@@ -64,7 +63,6 @@ class Autoencoder():
         self.loss = LOSS_TYPES[loss_type](**loss_args)
         self.outputs_metadata = out_modules
         self.model = AEModule()
-        self.norm_options = norm_options
 
         encoder = torch.nn.Sequential()
         self.model.add_module('encoder', encoder)
@@ -235,9 +233,6 @@ class Autoencoder():
                               rownames=rownames if rname else None,
                               colnames=colnames if cname else None)
         return preds
-
-    def normalize(self, X):
-        return io.normalize(X, self.norm_options)
 
 
 class ZINBAutoencoder(Autoencoder):