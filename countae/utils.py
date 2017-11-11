d308b8e9e14c6f5e4c6a7671c75a9b49069b4c38
Gökcen Eraslan <gokcen.eraslan@gmail.com>
2017-11-11 22:49:02 +0100

    Add fancy progress bar to ZINB as well

--- countae/utils.py
+++ countae/utils.py
@@ -317,8 +317,12 @@ def train(model_dict, loss_dict, model, loss, optimizer, epochs=1,
     loader = DataLoader(train_data, batch_size=batch_size, shuffle=shuffle)
     result = {'loss': [], 'model': model, 'early_stop': False}
 
-    for epoch in range(epochs):
+    if verbose:
+        it = trange(epochs)
+    else:
+        it = range(epochs)
 
+    for epoch in it:
         train_batch_losses = []
 
         for modeld, lossd in loader:
@@ -352,8 +356,8 @@ def train(model_dict, loss_dict, model, loss, optimizer, epochs=1,
                 result.setdefault('val_loss', []).append(l.data.numpy()[0])
 
         if verbose:
-            print('Epoch: %s, train_loss: %s, val_loss: %s' % (epoch+1, result['loss'][-1],
-                                                               result['val_loss'][-1] if val_data else '---'))
+            text = 'Epoch: %s training loss: %s val loss: %s' % ((i+1), ret['loss'][-1], ret['val_loss'][-1] if 'val_loss' in ret else  '---')
+            it.set_description(text)
 
         if scheduler:
             if val_data is not None:
