f98461b837e5c787fe6b0bbffef27f036e316007
Gökcen Eraslan <gokcen.eraslan@gmail.com>
2017-11-25 21:26:37 +0100

    Adapt simulator to new splatter version

--- data/simulation/simulate.R
+++ data/simulation/simulate.R
@@ -1,32 +1,29 @@
 # Warning! R 3.4 and Bioconductor 3.5 are required for splatter!
 # library(BiocInstaller)
 # biocLite('splatter')
-library(splatter)
+library(splatter) # requires splatter >= 1.2.0
 
 save.sim <- function(sim, dir) {
   counts     <- counts(sim)
-  truecounts <- get_exprs(sim, 'TrueCounts')
-  drp <- 'Dropout' %in% names(assayData(sim))
+  truecounts <- assays(sim)$TrueCounts
+  drp <- 'Dropout' %in% names(assays(sim))
   if (drp) {
-    dropout    <- get_exprs(sim, 'Dropout')
+    dropout    <- assays(sim)$Dropout
     mode(dropout) <- 'integer'
   }
-  lognorm <- get_exprs(sim, 'exprs')
-  cellinfo   <- pData(sim)
-  geneinfo   <- fData(sim)
+  cellinfo   <- colData(sim)
+  geneinfo   <- rowData(sim)
 
   # save count matrices
   write.table(counts, paste0(dir, '/counts.tsv'),
-              sep='\t', row.names=F, col.names=F, quote=F)
+              sep='\t', row.names=T, col.names=T, quote=F)
   write.table(truecounts, paste0(dir, '/info_truecounts.tsv'),
-              sep='\t', row.names=F, col.names=F, quote=F)
-  write.table(format(lognorm, digits=6), paste0(dir, '/counts_lognorm.tsv'),
-              sep='\t', row.names=F, col.names=F, quote=F)
+              sep='\t', row.names=T, col.names=T, quote=F)
 
   if (drp) {
     # save ground truth dropout labels
     write.table(dropout, paste0(dir, '/info_droupout.tsv'),
-                sep='\t', row.names=F, col.names=F, quote=F)
+                sep='\t', row.names=T, col.names=T, quote=F)
   }
 
   # save metadata
@@ -44,19 +41,19 @@ for (dropout in c(0, 1, 3, 5)) {
     for(swap in c(F, T)) {
 
       nGenes <- 200
-      nCells <- 2000
+      batchCells <- 2000
 
       if (swap) {
         tmp <- nGenes
-        nGenes <- nCells
-        nCells <- tmp
+        nGenes <- batchCells
+        batchCells <- tmp
       }
 
       # split nCells into roughly ngroup groups
       if(ngroup==1) {
-        groupCells <- nCells
+        group.prob <- 1
       } else {
-        groupCells <- as.vector(table(as.integer(cut(seq_len(nCells), ngroup))))
+        group.prob <- rep(1, ngroup)/ngroup
       }
       method <- ifelse(ngroup == 1, 'single', 'groups')
 
@@ -69,7 +66,7 @@ for (dropout in c(0, 1, 3, 5)) {
       params <- splatEstimate(sc_example_counts)
 
       # simulate scRNA data
-      sim <- splatSimulate(params, groupCells=groupCells, nGenes=nGenes,
+      sim <- splatSimulate(params, group.prob=group.prob, nGenes=nGenes,
                            dropout.present=(dropout!=0), dropout.shape=-1,
                            dropout.mid=dropout, seed=42, method=method,
                            bcv.common=1) # limit disp to get
@@ -82,7 +79,7 @@ for (dropout in c(0, 1, 3, 5)) {
         dir.create(dirname, showWarnings=F, recursive=T)
 
       #### Simulate data without using real data
-      sim <- splatSimulate(groupCells=groupCells, nGenes=nGenes,
+      sim <- splatSimulate(group.prob=group.prob, nGenes=nGenes, batchCells=batchCells,
                            dropout.present=(dropout!=0), method=method,
                            seed=42, dropout.shape=-1, dropout.mid=dropout)
       save.sim(sim, dirname)
