7a7bbff7f11cd70f333acf77d082a2c734a8c784
Gökcen Eraslan <goekcen.eraslan@helmholtz-muenchen.de>
2018-04-30 11:47:41 +0200

    Final steps for pypi deployment

--- setup.py
+++ setup.py
@@ -1,23 +1,13 @@
 from setuptools import setup
-from subprocess import check_output, CalledProcessError
-
-try:
-    num_gpus = len(check_output(['nvidia-smi', '--query-gpu=gpu_name',
-                                 '--format=csv']).decode().strip().split('\n'))
-    tf = 'tensorflow-gpu>=1.0.0' if num_gpus > 1 else 'tensorflow>=1.0.0'
-except (CalledProcessError, FileNotFoundError, OSError):
-    tf = 'tensorflow>=1.0.0'
-
 
 setup(
     name='DCA',
     version='0.1',
     description='Count autoencoder for scRNA-seq denoising',
     author='Gokcen Eraslan',
-    author_email="goekcen.eraslan@helmholtz-muenchen.de",
+    author_email="gokcen.eraslan@gmail.com",
     packages=['DCA'],
-    install_requires=[tf,
-                      'numpy>=1.7',
+    install_requires=['numpy>=1.7',
                       'keras>=2.0.8',
                       'h5py',
                       'six>=1.10.0',
