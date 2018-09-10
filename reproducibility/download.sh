6b6bf39a2d4bbc87bb30bbd1d826a7e9df6effc3
Gökcen Eraslan <gokcen.eraslan@gmail.com>
2018-09-10 15:02:40 +0200

    Extract data files to data folder

--- reproducibility/download.sh
+++ reproducibility/download.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# downloads 722M dataset file
 wget https://hmgubox.helmholtz-muenchen.de/f/1a014dc377f64b2b964c/?dl=1 -O datasets.zip
-unzip datasets.zip
+mkdir data; cd data
+unzip ../datasets.zip
