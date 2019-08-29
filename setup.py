42df1fe9bb32dcebc1ab396689170e8cea794bfb
Gökcen Eraslan <geraslan@broadinstitute.org>
2019-08-29 15:03:44 -0400

    New release with configurable LR

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='DCA',
-    version='0.2.2',
+    version='0.2.3',
     description='Count autoencoder for scRNA-seq denoising',
     author='Gokcen Eraslan',
     author_email="gokcen.eraslan@gmail.com",
