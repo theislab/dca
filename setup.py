63db5baf1c23e49afd8bc4d695d9cfb650ce5625
Gokcen Eraslan <geraslan@broadinstitute.org>
2021-09-16 14:24:03 -0400

    DCA is not compatible with keras 2.6.x

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='DCA',
-    version='0.3.2',
+    version='0.3.3',
     description='Count autoencoder for scRNA-seq denoising',
     author='Gokcen Eraslan',
     author_email="gokcen.eraslan@gmail.com",
     packages=['dca'],
     install_requires=['numpy>=1.7',
-                      'keras>=2.4',
+                      'keras>=2.4,<2.6',
                       'tensorflow>=2.0',
                       'h5py',
                       'six>=1.10.0',
